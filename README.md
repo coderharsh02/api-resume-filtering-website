04cc2f6c53cf9405c348a34b9a8bc533ba10b949
Harsh Shah <harshshah.19.cs@iite.indusuni.ac.in>
2022-10-07 13:10:54 +0530

    updated group and link details

---
+++ README.md
@@ -0,0 +1,8 @@
+// # Resume Filtering Website (Project Semester 7)
+// # Group Member Details
+// # Member1 : Harsh Shah (IU1941230147)
+// # Member2 : Runil Patel (IU1941230140)
+// ##### Project URLs  
+// # GitHub Repository: https://github.com/coderharsh02/api-resume-filtering-website
+// # Heroku URL:        https://resume-filtering-web.herokuapp.com/api/applicant
+////////////////////////////////////////////////////////////////////////////////////
--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+// # Resume Filtering Website (Project Semester 7)
+// # Group Member Details
+// # Member1 : Harsh Shah (IU1941230147)
+// # Member2 : Runil Patel (IU1941230140)
+// ##### Project URLs  
+// # GitHub Repository: https://github.com/coderharsh02/api-resume-filtering-website
+// # Heroku URL:        https://resume-filtering-web.herokuapp.com/api/applicant
+////////////////////////////////////////////////////////////////////////////////////
+
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
